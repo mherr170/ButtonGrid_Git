6d70f9030cb1a1f7f1ebca78787df73a9b94ab18
mherring <mherr170@gmail.com>
2020-07-07 19:03:32 -0400

    Additional Refactoring, and more iterations regarding the Land generation.

--- ButtonGrid_Git/TerrainGeneration/GenerateTerrain.cs
+++ ButtonGrid_Git/TerrainGeneration/GenerateTerrain.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Windows.Media;
 using ButtonGrid_Git.DTO;
 using ButtonGrid_Git.Enum;
 using ButtonGrid_Git.Constants;
+using ButtonGrid_Git.Utility.RandomNumber;
+using System.Security.Cryptography.X509Certificates;
+using System.Linq;
 using System.Windows.Controls;
 
 namespace ButtonGrid_Git.TerrainGeneration
 {
     class GenerateTerrain
     {
-        private Random _random = new Random();
-
         public GenerateTerrain(GridButton[,] gridButtonMultiArray)
         {
             TerrainTypeEnum.InitTerrainTypeDictionary();
@@ -23,71 +23,195 @@ namespace ButtonGrid_Git.TerrainGeneration
 
         private void GenerateLandCheck(GridButton[,] gridButtonMultiArray)
         {
-            //Randomly select a pre-determined number of tiles to swap to land and serve as the starting point for the terrain generation.
-            List<GridButton> randomTileList = PickRandomStartingTiles(gridButtonMultiArray);
+            #region Variable Declaration
 
-            //This list stores the GridButtons that, if valid, are adjacent to the randomTileList.
-            List<GridButton> tileAdjacencyList = new List<GridButton>();
+            //Randomly select a pre-determined number of tiles to swap to land and serve as the starting point for the terrain generation.
+            List<GridButton> startingPointRandomTileList = PickRandomStartingTiles(gridButtonMultiArray);
+
+            //This list stores the GridButtons that are adjacent to the randomTileList.
+            List<GridButton> tileAdjacencyListFirstIteration = new List<GridButton>();
 
             //The list to store GridButtons that are adjacent to the GridButtons within the tileAdjacencyList (first iteration)
             List<GridButton> tileAdjacencyListSecondIteration = new List<GridButton>();
 
-            //Swap the randomly selected tiles to Land.
-            SwapTilesToLand(randomTileList);
+            //The list to store GridButtons that are adjacent to the GridButtons within the tileAdjacencyListSecondIteration
+            List<GridButton> tileAdjacencyListThirdIteration = new List<GridButton>();
 
-            //The first iteration where the adjacent buttons have an 80% chance to be swapped to Land.
-            GatherAdjacentTilesFirstIteration(gridButtonMultiArray, randomTileList, tileAdjacencyList);
+            //The list to store GridButtons that are adjacent to the GridButtons within the tileAdjacencyListThirdIteration
+            List<GridButton> tileAdjacencyListFourthIteration = new List<GridButton>();
 
-            CalculateAndSwapFirstIteration(tileAdjacencyList);
+            #endregion
 
-            //Need to populate another adjacency list, from the tileAdjacencyList.
-            PopulateSecondTierAdjacencyList(gridButtonMultiArray, tileAdjacencyList, tileAdjacencyListSecondIteration);
+            //Swap the initial randomly selected tiles to Land.
+            SwapTilesToLand(startingPointRandomTileList);
 
-            //The second interation where the tiles within the tileAdjacencyList have 40% chance to be swapped to Land.
-            CalculateAndSwapSecondIteration(tileAdjacencyListSecondIteration);
+            //Gather adjacent tiles, remove duplicates and land, calculate swap to land.
+            FirstIteration(gridButtonMultiArray, startingPointRandomTileList, ref tileAdjacencyListFirstIteration);
+
+            SecondIteration(gridButtonMultiArray, tileAdjacencyListFirstIteration, ref tileAdjacencyListSecondIteration);
+
+            ThirdIteration(gridButtonMultiArray, tileAdjacencyListSecondIteration, ref tileAdjacencyListThirdIteration);
+
+            FourthIteration(gridButtonMultiArray, tileAdjacencyListThirdIteration, ref tileAdjacencyListFourthIteration);
         }
 
-        private void PopulateSecondTierAdjacencyList(GridButton[,] gridButtonMultiArray, List<GridButton> tileAdjacencyList, List<GridButton> tileAdjacencyListSecondIteration)
+        private void FirstIteration(GridButton[,] gridButtonMultiArray, List<GridButton> startingPointRandomTileList, ref List<GridButton> tileAdjacencyListFirstIteration)
         {
-            foreach (GridButton adjacentButton in tileAdjacencyList)
+            //Gather the tiles that are adjacent to the startingPointRandomTileList
+            GatherAdjacentTiles(gridButtonMultiArray, startingPointRandomTileList, tileAdjacencyListFirstIteration);
+
+            //Remove any possible duplicate GridButtons within the list.
+            tileAdjacencyListFirstIteration = tileAdjacencyListFirstIteration.Distinct().ToList();
+
+            //Unlikely at this point in the process, but remove any tiles that have been swapped to land into a temp list.  Don't need to calculate for these.
+            //Doing this into a temporary list as to keep the data intact within the original "tileAdjacencyListFirstIteration" list.
+            List<GridButton> tempGridButtonList = RemoveTilesThatHaveBeenSwappedToLand(tileAdjacencyListFirstIteration);
+
+            //For the First Iteration, the adjacent tiles have an 80% chance to be swapped from Water to Land.
+            CalculateAndSwapFirstIteration(tempGridButtonList);
+        }
+
+        private void SecondIteration(GridButton[,] gridButtonMultiArray, List<GridButton> tileAdjacencyListFirstIteration, ref List<GridButton> tileAdjacencyListSecondIteration)
+        {
+            //Populate the tiles that are adjacent to those within the tileAdjacencyListFirstIteration.
+            GatherAdjacentTiles(gridButtonMultiArray, tileAdjacencyListFirstIteration, tileAdjacencyListSecondIteration);
+
+            //Remove any possible duplicate GridButtons within the grid.
+            tileAdjacencyListSecondIteration = tileAdjacencyListSecondIteration.Distinct().ToList();
+
+            //By this point, within tileAdjacencyListSecondIteration there are tiles being collected that have already been swapped to Land from previous iterations or from the starting point.
+            //Remove them into a temporary list.  Not removing from the main list, because that would interfere with the next iteration's adjacent tile gathering.
+            List<GridButton> tempGridButtonList = RemoveTilesThatHaveBeenSwappedToLand(tileAdjacencyListSecondIteration);
+
+            //The second interation where the tiles within the tileAdjacencyListSecondIteration have 60% chance to be swapped to Land.
+            CalculateAndSwapSecondIteration(tempGridButtonList);
+        }
+
+        private void ThirdIteration(GridButton[,] gridButtonMultiArray, List<GridButton> tileAdjacencyListSecondIteration, ref List<GridButton> tileAdjacencyListThirdIteration)
+        {
+            //Populate the tiles that are adjacent to those within the tileAdjacencyListSecondIteration
+            GatherAdjacentTiles(gridButtonMultiArray, tileAdjacencyListSecondIteration, tileAdjacencyListThirdIteration);
+
+            tileAdjacencyListThirdIteration = tileAdjacencyListThirdIteration.Distinct().ToList();
+
+            //Remove the tiles that have been swapped to land again here from the tileAdjacencyListThirdIteration list.
+            List<GridButton> tempGridButtonList = RemoveTilesThatHaveBeenSwappedToLand(tileAdjacencyListThirdIteration);
+
+            //The third iteration where the tiles within the tileAdjacencyListThirdIteration have a 40% chance to be swapped to Land.
+            CalculateAndSwapThirdIteration(tempGridButtonList);
+        }
+
+        private void FourthIteration(GridButton[,] gridButtonMultiArray, List<GridButton> tileAdjacencyListThirdIteration, ref List<GridButton> tileAdjacencyListFourthIteration)
+        {
+            GatherAdjacentTiles(gridButtonMultiArray, tileAdjacencyListThirdIteration, tileAdjacencyListFourthIteration);
+
+            tileAdjacencyListFourthIteration = tileAdjacencyListFourthIteration.Distinct().ToList();
+
+            List<GridButton> tempGridButtonList = RemoveTilesThatHaveBeenSwappedToLand(tileAdjacencyListFourthIteration);
+
+            CalculateAndSwapFourthIteration(tempGridButtonList);
+        }
+
+        private List<GridButton> RemoveTilesThatHaveBeenSwappedToLand(List<GridButton> tileAdjacencyListFirstIteration)
+        {
+            List<GridButton> temporaryDuplicatesRemovalList = new List<GridButton>(tileAdjacencyListFirstIteration);
+
+            temporaryDuplicatesRemovalList.RemoveAll(x => x.HasBeenSwappedToLand == true);
+
+            return temporaryDuplicatesRemovalList;
+        }
+
+        private void CalculateAndSwapFourthIteration(List<GridButton> tileAdjacencyListFourthIteration)
+        { 
+            foreach (GridButton gridButton in tileAdjacencyListFourthIteration)
             {
-                if (adjacentButton.IsTopAdjacencyValid)
+                CalculateTwentyPercentChanceToSwitchToLand(gridButton);
+            }
+        }
+
+        private void CalculateTwentyPercentChanceToSwitchToLand(GridButton gridButton)
+        {
+            //There is an 80% chance to switch the adjacent tiles from "Water" to "Land"
+            switch (RandomNumberGenerator.GetNextRandomNumberBetween1and5())
+            {
+                //If a "1" has been generated, this is the 20% chance to swap to Land.
+                case 1:
+                    gridButton.GridButtonTerrainType = TerrainTypeEnum.TerrainType.Land;
+                    gridButton.HasBeenSwappedToLand = true;
+                    break;
+                //Else, a "2", "3", "4", or "5" was generated.
+                default:
+
+                    break;
+            }
+        }
+
+        private void CalculateAndSwapThirdIteration(List<GridButton> tileAdjacencyListThirdIteration)
+        {
+            foreach (GridButton gridButton in tileAdjacencyListThirdIteration)
+            {
+                if (!gridButton.HasBeenSwappedToLand)
+                { 
+                    CalculateFortyPercentChanceAndSwitchToLand(gridButton);
+                }
+            }            
+        }
+
+        private void CalculateFortyPercentChanceAndSwitchToLand(GridButton gridButton)
+        {
+            //There is an 80% chance to switch the adjacent tiles from "Water" to "Land"
+            switch (RandomNumberGenerator.GetNextRandomNumberBetween1and5())
+            {
+                //If a "1" or "2" has been generated, this is the 40% to swap to Land.
+                case 1:
+                case 2:
+                    gridButton.GridButtonTerrainType = TerrainTypeEnum.TerrainType.Land;
+                    gridButton.HasBeenSwappedToLand = true;
+                    break;
+                //Else, a "3", "4", or "5" was generated.
+                default:
+
+                    break;
+            }
+        }
+
+        private void GatherAdjacentTiles(GridButton[,] gridButtonMultiArray, List<GridButton> sourceList, List<GridButton> childList)
+        {
+            foreach (GridButton gridButton in sourceList)
+            {
+                if (gridButton.IsTopAdjacencyValid)
                 {
-                    tileAdjacencyListSecondIteration.Add(gridButtonMultiArray[adjacentButton.TopAdjacency.RowNumber, adjacentButton.TopAdjacency.ColumnNumber]);
+                    childList.Add(gridButtonMultiArray[gridButton.TopAdjacency.RowNumber, gridButton.TopAdjacency.ColumnNumber]);
                 }
 
-                if (adjacentButton.IsDownAdjacencyValid)
+                if (gridButton.IsDownAdjacencyValid)
                 {
-                    tileAdjacencyListSecondIteration.Add(gridButtonMultiArray[adjacentButton.DownAdjacency.RowNumber, adjacentButton.DownAdjacency.ColumnNumber]);
+                    childList.Add(gridButtonMultiArray[gridButton.DownAdjacency.RowNumber, gridButton.DownAdjacency.ColumnNumber]);
                 }
 
-                if (adjacentButton.IsLeftAdjacencyValid)
+                if (gridButton.IsLeftAdjacencyValid)
                 {
-                    tileAdjacencyListSecondIteration.Add(gridButtonMultiArray[adjacentButton.LeftAdjacency.RowNumber, adjacentButton.LeftAdjacency.ColumnNumber]);
+                    childList.Add(gridButtonMultiArray[gridButton.LeftAdjacency.RowNumber, gridButton.LeftAdjacency.ColumnNumber]);
                 }
 
-                if (adjacentButton.IsRightAdjacencyValid)
+                if (gridButton.IsRightAdjacencyValid)
                 {
-                    tileAdjacencyListSecondIteration.Add(gridButtonMultiArray[adjacentButton.RightAdjacency.RowNumber, adjacentButton.RightAdjacency.ColumnNumber]);
+                    childList.Add(gridButtonMultiArray[gridButton.RightAdjacency.RowNumber, gridButton.RightAdjacency.ColumnNumber]);
                 }
             }
         }
 
-        private void CalculateAndSwapSecondIteration(List<GridButton> tileAdjacencyList)
+        private void CalculateAndSwapSecondIteration(List<GridButton> tileAdjacencyListSecondIteration)
         {
-            foreach (GridButton adjacentTile in tileAdjacencyList)
-            {
+            foreach (GridButton adjacentTile in tileAdjacencyListSecondIteration)
+            {        
                 CalculateSixtyPercentChanceAndSwitchToLand(adjacentTile);
             }
         }
 
         private void CalculateSixtyPercentChanceAndSwitchToLand(GridButton adjacentTile)
         {
-            int randomOutcome;
-
-            randomOutcome = _random.Next(1, VariableConstants.SELECT_RANDOM_TILES_NUMBER + 1);
-
-            switch (randomOutcome)
+            switch (RandomNumberGenerator.GetNextRandomNumberBetween1and5())
             {
                 //Case 1,2, and 3 is a 60% chance to swap to Land.
                 case 1:
@@ -101,40 +225,9 @@ namespace ButtonGrid_Git.TerrainGeneration
             }
         }
 
-        private void GatherAdjacentTilesFirstIteration(GridButton[,] gridButtonMultiArray, List<GridButton> randomTileList, List<GridButton> tileAdjacencyList)
+        private void CalculateAndSwapFirstIteration(List<GridButton> tileAdjacencyListFirstIteration)
         {
-            //Calculate percentage and swap to land if necessary for the five initially picked GridButtons.
-            foreach (GridButton randomGridButton in randomTileList)
-            {
-                if (randomGridButton.IsTopAdjacencyValid)
-                {
-                    //Add the valid adjacent button to the tileAdjacencyList for calculating on the next iteration.
-                    tileAdjacencyList.Add(gridButtonMultiArray[randomGridButton.TopAdjacency.RowNumber, randomGridButton.TopAdjacency.ColumnNumber]);
-                }
-
-                if (randomGridButton.IsDownAdjacencyValid)
-                {
-                    //Add the valid adjacent button to the tileAdjacencyList for calculating on the next iteration.
-                    tileAdjacencyList.Add(gridButtonMultiArray[randomGridButton.DownAdjacency.RowNumber, randomGridButton.DownAdjacency.ColumnNumber]);
-                }
-
-                if (randomGridButton.IsLeftAdjacencyValid)
-                {
-                    //Add the valid adjacent button to the tileAdjacencyList for calculating on the next iteration.
-                    tileAdjacencyList.Add(gridButtonMultiArray[randomGridButton.LeftAdjacency.RowNumber, randomGridButton.LeftAdjacency.ColumnNumber]);
-                }
-
-                if (randomGridButton.IsRightAdjacencyValid)
-                {
-                    //Add the valid adjacent button to the tileAdjacencyList for calculating on the next iteration.
-                    tileAdjacencyList.Add(gridButtonMultiArray[randomGridButton.RightAdjacency.RowNumber, randomGridButton.RightAdjacency.ColumnNumber]);
-                }
-            }
-        }
-
-        private void CalculateAndSwapFirstIteration(List<GridButton> tileAdjacencyList)
-        {
-            foreach (GridButton gridButton in tileAdjacencyList)
+            foreach (GridButton gridButton in tileAdjacencyListFirstIteration)
             {
                 CalculateEightyPercentChanceAndSwitchToLand(gridButton);
             }
@@ -142,12 +235,8 @@ namespace ButtonGrid_Git.TerrainGeneration
 
         private void CalculateEightyPercentChanceAndSwitchToLand(GridButton gridButton)
         {
-            int randomOutcome;
-
-            randomOutcome = _random.Next(1, VariableConstants.SELECT_RANDOM_TILES_NUMBER + 1);
-
             //There is an 80% chance to switch the adjacent tiles from "Water" to "Land"
-            switch (randomOutcome)
+            switch (RandomNumberGenerator.GetNextRandomNumberBetween1and5())
             {
                 //If a "5" has been generated, this is the 20% to keep it Water.
                 case 5:
@@ -178,15 +267,15 @@ namespace ButtonGrid_Git.TerrainGeneration
 
         private GridButton SelectRandomTile(GridButton[,] gridButtonMultiArray)
         {
-            int randomRow = _random.Next(0, Convert.ToInt32(VariableConstants.SQUARE_SIDE_LENGTH));
-            int randomColumn = _random.Next(0, Convert.ToInt32(VariableConstants.SQUARE_SIDE_LENGTH));
+            int randomRow = RandomNumberGenerator.GetNextRandomNumberBetween0andSQUARE_SIDE_LENGTH();
+            int randomColumn = RandomNumberGenerator.GetNextRandomNumberBetween0andSQUARE_SIDE_LENGTH();
 
             return gridButtonMultiArray[randomRow, randomColumn];
         }
 
-        private void SwapTilesToLand(List<GridButton> randomTileList)
+        private void SwapTilesToLand(List<GridButton> startingPointRandomTileList)
         {
-            foreach (GridButton gridButton in randomTileList)
+            foreach (GridButton gridButton in startingPointRandomTileList)
             {
                 gridButton.GridButtonTerrainType = TerrainTypeEnum.TerrainType.Land;
                 gridButton.HasBeenSwappedToLand = true;
@@ -202,6 +291,11 @@ namespace ButtonGrid_Git.TerrainGeneration
                     gridButtonMultiArray[rowNumber, columnNumber].GridButtonTerrainType = TerrainTypeEnum.TerrainType.Water;
                 }
             }
+        }
+
+        private void RemoveDuplicateGridButtons()
+        {
+
         }
 
     }
---
+++ ButtonGrid_Git/Utility/RandomNumberGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using ButtonGrid_Git.Constants;
+
+namespace ButtonGrid_Git.Utility.RandomNumber
+{
+    public static class RandomNumberGenerator
+    {
+        static Random nextRandomNumber = new Random();
+
+        public static int GetNextRandomNumberBetween1and5()
+        {
+            return nextRandomNumber.Next(1, 6);
+        }
+
+        public static int GetNextRandomNumberBetween0andSQUARE_SIDE_LENGTH()
+        {
+            return nextRandomNumber.Next(0, Convert.ToInt32(VariableConstants.SQUARE_SIDE_LENGTH));
+        }
+    }
+}
